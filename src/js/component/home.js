7f720f15ae07b17e3674102053d46088cd251585
MLopez8282 <saltairpix@gmail.com>
2020-12-15 03:36:26 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,10 +8,23 @@ import tree from "../../img/tree.png";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<img className="farting-dog" src={dog}  />
-			<img className="livingroom" src={livingroom} />
-			<img className="tree" src={tree} />
+		<div className="text-center">
+			<img
+				className="farting-dog"
+				src={dog}
+				style={{ height: 360, width: 380 }}
+			/>
+
+			<img
+				className="tree"
+				src={tree}
+				style={{ width: 500, height: 560 }}
+			/>
+			<img
+				className="livingroom"
+				src={livingroom}
+				style={{ width: 1450, height: 700 }}
+			/>
 		</div>
 	);
 }
